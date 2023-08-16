90284aeeb0b951e76cafd81b28e8bfaa40590544
rayhan <rayhananthaakbar@gmail.com>
2023-08-16 08:58:15 +0700

    update: learn post & put

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.post('/login', (req, res) => {
+    console.log({ requestFromOutside: req.body });
+    res.send('login succesfull')
+})
+
+app.put('/profile', (req, res) => {
+    console.log({ updateData: req.body });
+    res.send('profile updated')
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
