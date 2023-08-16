c4fae571d2cf2b2ed3f40fb99ca7492f51130794
rayhan <rayhananthaakbar@gmail.com>
2023-08-16 20:57:44 +0700

    update: display searched data

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
+const db = require('./connect')
+const response = require('./response')
 
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-    console.log({ urlParam: req.query });
-    res.send('Hello World!')
+    const query = "SELECT * FROM mahasiswa"
+
+    db.query(query, (err, result) => {
+        // display data
+        response(200, result, "get all data from 'mahasiswa'", res)
+    })
+})
+
+app.get('/nim', (req, res) => {
+    console.log(`find nim: ${req.query.nim}`)
+
+    const query = `SELECT nama FROM mahasiswa WHERE nim = ${req.query.nim}`
+    db.query(query, (err, result) => {
+        response(200, result, "find mahasiswa name", res)
+    })
 })
 
 app.post('/login', (req, res) => {
