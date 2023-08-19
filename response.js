e3d591aa583c1bbe37392a2a5cc1dae7760b1cc3
rayhan <rayhananthaakbar@gmail.com>
2023-08-19 11:07:20 +0700

    modified: response structure

--- response.js
+++ response.js
@@ -1,15 +1,16 @@
 const response = (statusCode, data, message, res) => {
-    res.status(statusCode).json({
-        payload: {
-            status_code: statusCode,
-            datas: data,
-        },
-        message: message,
-        metadata: {
-            prev: "",
-            next: "",
-            current: ""
+    res.json(statusCode, [
+        {
+            payload: {
+                data,
+                message
+            },
+            metadata: {
+                prev: "",
+                next: "",
+                current: "",
+            }
         }
-    })
+    ])
 }
 module.exports = response
