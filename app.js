af19e3c623efa6ee270f2661485e22445d128ae4
rayhan <rayhananthaakbar@gmail.com>
2023-08-26 19:06:41 +0700

    update: add middleware and new api routes

--- app.js
+++ app.js
@@ -1,87 +1,142 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const APP = express()
+const PORT = 500
+
 const bodyParser = require('body-parser')
 const db = require('./connect') // connect db config
 const response = require('./response') // response on query config
 
 // parser to help logs can read json format
-app.use(bodyParser.json())
+APP.use(bodyParser.json())
+
+// setup middleware
+const middleware = (req, res, next) => {
+    // you need "hjK23Wr" key to access data from api
+    if (req.query.key == 'hjK23Wr') {
+        console.log('[Success 200]: API request sent');
+        next()
+    } else {
+        res.send('[Error 403]: API request forbidden')
+    }
+}
+
+// set a middleware to be use in every api request
+APP.use(middleware);
 
 // displaying all data from db
-app.get('/', (req, res) => {
+APP.get('/mahasiswa', (req, res) => {
     // sql to display all data
     const query = "SELECT * FROM mahasiswa"
 
-    db.query(query, (err, result) => {
+    db.query(query, (err, fields) => {
         // display data from query
-        response(200, result, "get all data from 'mahasiswa'", res)
+        if (err) throw response(500, "invalid", "error displaying data", res)
+        response(200, fields, "get all data from 'mahasiswa'", res)
     })
 })
 
-// display id by taking id from url
-app.get('/:id', (req, res) => {
+// display nim by taking nim from url
+APP.get('/mahasiswa/:nim', (req, res) => {
     // catch id from url
-    const id = req.params.id
-
-    // display id from 'id'
-    res.send(`DISPLAY ID: ${id}`)
-})
-
-// find a name from given nim 1
-// example: http://localhost:3000/find/nim=10100001 (find a name with nim 10100001)
-app.get('/find/nim=:nim', (req, res) => {
-    // catch nim from url
     const nim = req.params.nim
 
-    // sql to display name from mahasiswa by given nim
-    const query = `SELECT nama FROM mahasiswa WHERE nim = ${nim}`
+    // query to display name by given nim
+    const query = `SELECT * FROM mahasiswa WHERE nim = ${nim}`
 
-    // display name from given nim on url
-    db.query(query, (err, result) => {
-        response(200, result, "find mahasiswa name", res)
+    // display name by given nim
+    db.query(query, (err, fields) => {
+        if (err) throw response(500, "invalid", "error searching data", res)
+        if (fields != '') {
+            response(200, fields, `Get data by nim: ${nim}`, res)
+        }
+        else {
+            response(404, "Not Found", "error: data not found or not exist", res)
+        }
+    })
+})
+
+APP.post('/mahasiswa', (req, res) => {
+    // get data from input
+    const { nim, nama, alamat } = req.body
+
+    // query to insert data
+    const query = `INSERT INTO mahasiswa (nim, nama, alamat) VALUES
+    (${nim}, '${nama}', '${alamat}')`
+
+    // insert data to db
+    db.query(query, (err, fields) => {
+        if (err) throw response(500, "invalid", "error inserting data", res)
+        if (fields?.affectedRows) {
+            const data = {
+                isSuccess: fields.affectedRows,
+                id: fields.insertId
+            }
+            response(200, data, "data inserted successfully", res)
+        }
+    })
+})
+
+APP.put('/mahasiswa', (req, res) => {
+    // get data from input
+    const { nim, namaBaru } = req.body
+
+    // query to insert data
+    const query = `UPDATE mahasiswa SET nama='${namaBaru}' WHERE nim=${nim}`
+
+    // insert data to db
+    db.query(query, (err, fields) => {
+        if (err) throw err
+        if (err) throw response(500, "invalid", "error updating data", res)
+        if (fields?.affectedRows) {
+            const data = {
+                isUpdated: fields.affectedRows,
+                rowsUpdated: fields.changedRows,
+                message: fields.message
+            }
+            response(200, data, "data updated successfully", res)
+        } else {
+            response(404, "data not found or not exist", "error", res)
+        }
+    })
+})
+
+APP.delete('/mahasiswa', (req, res) => {
+    // get nim data
+    const { nim } = req.body
+
+    // query to delete given nim
+    const query = `DELETE FROM mahasiswa WHERE nim=${nim}`
+
+    // delete data from db with given nim
+    db.query(query, (err, fields) => {
+        if (err) throw response(500, "invalid", "error", res)
+        if (fields?.affectedRows) {
+            const data = {
+                isDeleted: fields.affectedRows
+            }
+            response(200, data, "data deleted successfully", res)
+        } else {
+            response(404, "data not found or not exist", "error", res)
+        }
     })
 })
 
 // find a name from given nim 2
 // example: http://localhost:3000/nim?nim=10100001 (find a name with nim 10100001)
-app.get('/nim', (req, res) => {
-    // display log of nim from query
-    console.log(`find nim: ${req.query.nim}`)
+// APP.get('/nim', (req, res) => {
+//     // display log of nim from query
+//     console.log(`find nim: ${req.query.nim}`)
 
-    // sql to display name from mahasiswa by given nim
-    const query = `SELECT nama FROM mahasiswa WHERE nim = ${req.query.nim}`
+//     // sql to display name from mahasiswa by given nim
+//     const query = `SELECT nama FROM mahasiswa WHERE nim = ${req.query.nim}`
 
-    // display mahasiswa from query by nim
-    db.query(query, (err, result) => {
-        response(200, result, "find mahasiswa name", res)
-    })
-})
-
-// testing update data (untested)
-// app.post('/update-nim', (req, res) => {
-//     console.log(`update data: ${req.query.nama}, ${req.query.nim}`)
-
-//     const query = `UPDATE mahasiswa 
-//     SET nama='${req.query.nama}' where id=${req.query.nim}`
-//     db.query(query, (err, result) => {
-//         response(200, result, "update mahasiswa name", res)
+//     // display mahasiswa from query by nim
+//     db.query(query, (err, fields) => {
+//         response(200, fields, "find mahasiswa name", res)
 //     })
 // })
 
-// testing log from method post
-app.post('/login', (req, res) => {
-    console.log({ requestFromOutside: req.body });
-    res.send('login succesfull')
-})
-
-// testing log from method put
-app.put('/profile', (req, res) => {
-    console.log({ updateData: req.body });
-    res.send('profile updated')
-})
-
-// app runner
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+// run APP
+APP.listen(PORT, () => {
+    console.log(`Starting app with port ${PORT}`)
 })
