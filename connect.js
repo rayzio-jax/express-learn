cb8e30b8145d6a77a41cb58b2f0b2990ce850eb1
rayhan <rayhananthaakbar@gmail.com>
2023-08-26 19:06:07 +0700

    update: create connection explanation

--- connect.js
+++ connect.js
@@ -1,10 +1,10 @@
 const sql = require('mysql')
 
 const db = sql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "demodbuniv"
+    host: "localhost", // your local ip / localhost
+    user: "root", // your db user (default = root)
+    password: "", // your db user password (default = none)
+    database: "demodbuniv" // your database
 })
 
 module.exports = db
